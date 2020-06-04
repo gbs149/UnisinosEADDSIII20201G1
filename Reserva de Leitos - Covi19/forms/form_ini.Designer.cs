c54361efac262653769d4b0fe80e64db6062edfd
Ivan Veiverberg <ivanveiverberg@hotmail.com>
2020-06-03 22:17:27 -0300

    - Retirado espaço (Tela de Cadastro de Hospitais) - Ajustado Letra para maiusculo form inicial.

--- Reserva de Leitos - Covi19/forms/form_ini.Designer.cs
+++ Reserva de Leitos - Covi19/forms/form_ini.Designer.cs
@@ -41,9 +41,11 @@
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(8, 8);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(794, 440);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(529, 286);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Consulta de Leitos - Covid 19";
@@ -51,20 +53,22 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(519, 157);
+            this.button3.Location = new System.Drawing.Point(346, 102);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(161, 116);
+            this.button3.Size = new System.Drawing.Size(107, 75);
             this.button3.TabIndex = 5;
-            this.button3.Text = "Gerencia de leitos";
+            this.button3.Text = "Gerencia de Leitos";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(309, 157);
+            this.button2.Location = new System.Drawing.Point(206, 102);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(161, 116);
+            this.button2.Size = new System.Drawing.Size(107, 75);
             this.button2.TabIndex = 4;
             this.button2.Text = "Gerencia de Hospitais";
             this.button2.UseVisualStyleBackColor = true;
@@ -73,9 +77,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(100, 157);
+            this.button1.Location = new System.Drawing.Point(67, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(161, 116);
+            this.button1.Size = new System.Drawing.Size(107, 75);
             this.button1.TabIndex = 3;
             this.button1.Text = "Gerencia de Pacientes";
             this.button1.UseVisualStyleBackColor = true;
@@ -83,10 +88,11 @@
             // 
             // form_ini
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(818, 464);
+            this.ClientSize = new System.Drawing.Size(545, 302);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "form_ini";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Consulta de Leitos - Covid 19";
