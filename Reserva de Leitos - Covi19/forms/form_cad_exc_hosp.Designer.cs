a97e7acbb3a302e381c1281fd334959d6f253190
Ivan Veiverberg <ivanveiverberg@hotmail.com>
2020-06-11 12:06:06 -0300

    - Retirado campo Quantidade de Leitos do cadastro do Hospital

--- Reserva de Leitos - Covi19/forms/form_cad_exc_hosp.Designer.cs
+++ Reserva de Leitos - Covi19/forms/form_cad_exc_hosp.Designer.cs
@@ -36,11 +36,9 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
@@ -55,17 +53,17 @@
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.textBox5);
-            this.groupBox1.Controls.Add(this.textBox4);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(11, 13);
+            this.groupBox1.Location = new System.Drawing.Point(7, 8);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(796, 439);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(531, 241);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Gerenciamento do cadastro de hospitais";
@@ -73,9 +71,10 @@
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(507, 118);
+            this.button6.Location = new System.Drawing.Point(338, 77);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(86, 35);
+            this.button6.Size = new System.Drawing.Size(57, 23);
             this.button6.TabIndex = 26;
             this.button6.Text = "buscar";
             this.button6.UseVisualStyleBackColor = true;
@@ -84,9 +83,10 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(697, 218);
+            this.button5.Location = new System.Drawing.Point(465, 112);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(86, 35);
+            this.button5.Size = new System.Drawing.Size(57, 23);
             this.button5.TabIndex = 25;
             this.button5.Text = "buscar";
             this.button5.UseVisualStyleBackColor = true;
@@ -94,9 +94,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(210, 314);
+            this.button4.Location = new System.Drawing.Point(140, 160);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(160, 96);
+            this.button4.Size = new System.Drawing.Size(107, 62);
             this.button4.TabIndex = 24;
             this.button4.Text = "Alterar";
             this.button4.UseVisualStyleBackColor = true;
@@ -104,9 +105,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(573, 314);
+            this.button3.Location = new System.Drawing.Point(382, 160);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(160, 96);
+            this.button3.Size = new System.Drawing.Size(107, 62);
             this.button3.TabIndex = 23;
             this.button3.Text = "Cancelar";
             this.button3.UseVisualStyleBackColor = true;
@@ -114,9 +116,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(391, 314);
+            this.button2.Location = new System.Drawing.Point(261, 160);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(160, 96);
+            this.button2.Size = new System.Drawing.Size(107, 62);
             this.button2.TabIndex = 22;
             this.button2.Text = "Excluir";
             this.button2.UseVisualStyleBackColor = true;
@@ -124,9 +127,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(31, 314);
+            this.button1.Location = new System.Drawing.Point(21, 160);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 96);
+            this.button1.Size = new System.Drawing.Size(107, 62);
             this.button1.TabIndex = 21;
             this.button1.Text = "Cadastrar";
             this.button1.UseVisualStyleBackColor = true;
@@ -134,74 +138,65 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(165, 218);
+            this.textBox5.Location = new System.Drawing.Point(110, 112);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(520, 35);
+            this.textBox5.Size = new System.Drawing.Size(348, 26);
             this.textBox5.TabIndex = 5;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(165, 168);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(121, 35);
-            this.textBox4.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(165, 118);
+            this.textBox2.Location = new System.Drawing.Point(110, 77);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(321, 35);
+            this.textBox2.Size = new System.Drawing.Size(215, 26);
             this.textBox2.TabIndex = 2;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(165, 67);
+            this.textBox1.Location = new System.Drawing.Point(110, 44);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(558, 35);
+            this.textBox1.Size = new System.Drawing.Size(373, 26);
             this.textBox1.TabIndex = 1;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(26, 221);
+            this.label5.Location = new System.Drawing.Point(17, 114);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(97, 29);
+            this.label5.Size = new System.Drawing.Size(65, 20);
             this.label5.TabIndex = 19;
             this.label5.Text = "Cidade";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(26, 171);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(140, 29);
-            this.label4.TabIndex = 18;
-            this.label4.Text = "Qtd. Leitos";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 121);
+            this.label2.Location = new System.Drawing.Point(17, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 29);
+            this.label2.Size = new System.Drawing.Size(53, 20);
             this.label2.TabIndex = 16;
             this.label2.Text = "CNPJ";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 70);
+            this.label1.Location = new System.Drawing.Point(17, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 29);
+            this.label1.Size = new System.Drawing.Size(55, 20);
             this.label1.TabIndex = 15;
             this.label1.Text = "Nome";
             // 
             // form_cad_exc_hosp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(818, 464);
+            this.ClientSize = new System.Drawing.Size(545, 256);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "form_cad_exc_hosp";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gerencia de Hospitais";
@@ -215,11 +210,9 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button4;
